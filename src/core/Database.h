ec3ec03f1ff679d3b6ed016e73848e6278604e94
mohammadmk47 <mohammedmk3900@gmail.com>
2026-08-11 22:33:16 +0330

    Refactor Database class to use GameRecord structure

--- src/core/Database.h
+++ src/core/Database.h
@@ -2,14 +2,25 @@
 #define DATABASE_H
 
 #include <QString>
-#include <QStringList>
+#include <QVariantList>
+#include <QSqlDatabase>
+
+struct GameRecord {
+    int id = -1;
+    QString name;
+    QString exePath;
+    QString iconPath;
+    QString platform;
+};
 
 class Database
 {
 public:
     static bool initialize();
-    static void addGame(const QString& name, const QString& exePath, const QString& iconPath);
-    static QStringList getAllGames();
+    static bool addGame(const GameRecord& game);
+    static bool addGamesBatch(const QVector<GameRecord>& games);
+    static QVector<GameRecord> getAllGames();
+    static bool removeGame(int id);
 };
 
 #endif // DATABASE_H
