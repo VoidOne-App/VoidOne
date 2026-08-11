304044c3bd6ffdb6145c113b4657ae5573a7e05d
mohammadmk47 <mohammedmk3900@gmail.com>
2026-08-11 22:36:12 +0330

    Refactor GameModel methods and improve data handling

--- src/core/GameModel.cpp
+++ src/core/GameModel.cpp
@@ -1,72 +1,70 @@
 #include "GameModel.h"
-#include "Database.h"
 #include <QDebug>
 
-GameModel::GameModel(QObject *parent)
-    : QAbstractListModel(parent)
-{
-}
+GameModel::GameModel(QObject *parent) : QAbstractListModel(parent) {}
 
-int GameModel::rowCount(const QModelIndex &parent) const
-{
-    if (parent.isValid())
-        return 0;
+int GameModel::rowCount(const QModelIndex &parent) const {
+    if (parent.isValid()) return 0;
     return m_games.size();
 }
 
-QVariant GameModel::data(const QModelIndex &index, int role) const
-{
+QVariant GameModel::data(const QModelIndex &index, int role) const {
     if (!index.isValid() || index.row() >= m_games.size())
         return QVariant();
 
-    const GameEntry &game = m_games.at(index.row());
+    const GameRecord &game = m_games.at(index.row());
 
     switch (role) {
-    case NameRole:
-        return game.name;
-    case ExePathRole:
-        return game.exePath;
-    case IconPathRole:
-        return game.iconPath;
-    default:
-        return QVariant();
+    case IdRole: return game.id;
+    case NameRole: return game.name;
+    case ExePathRole: return game.exePath;
+    case IconPathRole: return game.iconPath;
+    case PlatformRole: return game.platform;
+    default: return QVariant();
     }
 }
 
-QHash<int, QByteArray> GameModel::roleNames() const
-{
+QHash<int, QByteArray> GameModel::roleNames() const {
     QHash<int, QByteArray> roles;
+    roles[IdRole] = "id";
     roles[NameRole] = "name";
     roles[ExePathRole] = "exePath";
-    roles[IconPathRole] = "icon";
+    roles[IconPathRole] = "iconPath";
+    roles[PlatformRole] = "platform";
     return roles;
 }
 
-void GameModel::addGame(const QString &name, const QString &exePath, const QString &iconPath)
-{
-    beginInsertRows(QModelIndex(), m_games.size(), m_games.size());
-    GameEntry entry;
-    entry.name = name;
-    entry.exePath = exePath;
-    entry.iconPath = iconPath;
-    m_games.append(entry);
-    endInsertRows();
+void GameModel::loadGamesFromDatabase() {
+    beginResetModel();
+    m_games = Database::getAllGames();
+    endResetModel();
+    emit countChanged();
 }
 
-void GameModel::loadGamesFromDatabase()
-{
-    clearGames();
-    QStringList games = Database::getAllGames();
-    for (const QString &gameName : games) {
-        addGame(gameName, "", "");
+bool GameModel::addNewGame(const QString &name, const QString &exePath, const QString &iconPath) {
+    GameRecord game{-1, name, exePath, iconPath, "Custom"};
+    if (Database::addGame(game)) {
+        loadGamesFromDatabase();
+        return true;
     }
+    return false;
 }
 
-void GameModel::clearGames()
-{
-    if (!m_games.isEmpty()) {
-        beginRemoveRows(QModelIndex(), 0, m_games.size() - 1);
-        m_games.clear();
+bool GameModel::deleteGame(int id, int index) {
+    if (index < 0 || index >= m_games.size()) return false;
+    
+    if (Database::removeGame(id)) {
+        beginRemoveRows(QModelIndex(), index, index);
+        m_games.removeAt(index);
         endRemoveRows();
+        emit countChanged();
+        return true;
+    }
+    return false;
+}
+
+void GameModel::launchGame(const QString &exePath) {
+    if (!exePath.isEmpty()) {
+        QProcess::startDetached(exePath, QStringList());
     }
 }
