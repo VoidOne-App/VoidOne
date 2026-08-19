a2d51d30ec1b3f3a374c6ef8b190fccbeda0cbdc
mohammadmk47 <mohammedmk3900@gmail.com>
2026-08-19 14:53:05 +0330

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
+    qDebug() << "Step 1";
+
     QQmlApplicationEngine engine;
+
+    qDebug() << "Step 2";
 
     QObject::connect(
         &engine,
@@ -18,10 +23,18 @@ int main(int argc, char *argv[])
         Qt::QueuedConnection
     );
 
+    qDebug() << "Step 3";
+
     engine.loadFromModule("NeonLauncher", "Main");
 
-    if (engine.rootObjects().isEmpty())
+    qDebug() << "Step 4";
+
+    if (engine.rootObjects().isEmpty()) {
+        qDebug() << "Step 5";
         return -1;
+    }
+
+    qDebug() << "Step 6";
 
     return app.exec();
 }
